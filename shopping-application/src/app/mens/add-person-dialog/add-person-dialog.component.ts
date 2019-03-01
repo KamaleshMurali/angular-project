fad176ff50a839e72437a32dc9582c15fc4fd7ef
kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-01 13:20:12 +0530

    add-person-dialog component has been created with the files .html .scss .spec.ts .ts

---
+++ shopping-application/src/app/mens/add-person-dialog/add-person-dialog.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-person-dialog',
+  templateUrl: './add-person-dialog.component.html',
+  styleUrls: ['./add-person-dialog.component.scss']
+})
+export class AddPersonDialogComponent implements OnInit {
+
+    form: FormGroup;
+    formName: string;
+    action: string;
+
+    constructor(private formBuilder: FormBuilder,
+                private dialogRef: MatDialogRef<AddPersonDialogComponent>,
+                @Inject(MAT_DIALOG_DATA) private prePopulateData) { }
+
+    ngOnInit() {
+        this.formData();
+    }
+
+    submit(form) {
+        console.log(this.form.value);
+        this.dialogRef.close(form.value);
+    }
+
+    formData() {
+        if (this.prePopulateData) {
+            this.formName = 'Update Person';
+            this.action = 'Update';
+            this.form = this.formBuilder.group({
+                id: this.prePopulateData.persons.id ? this.prePopulateData.persons.id : '',
+                firstName: this.prePopulateData.persons.firstName ? this.prePopulateData.persons.firstName : '',
+                lastName: this.prePopulateData.persons.lastName ? this.prePopulateData.persons.lastName : '',
+                email: this.prePopulateData.persons.email ? this.prePopulateData.persons.email : '',
+                dob: this.prePopulateData.persons.dob ? this.prePopulateData.persons.dob : '',
+            });
+            // this.form.controls.id.disable();
+        } else {
+            this.formName = 'Add Person';
+            this.action = 'Add';
+            this.form = this.formBuilder.group({
+                id: ['', Validators.required],
+                firstName: ['', Validators.required],
+                lastName: ['', Validators.required],
+                email: ['', Validators.required],
+                dob: ['', Validators.required]
+            });
+        }
+    }
+}
