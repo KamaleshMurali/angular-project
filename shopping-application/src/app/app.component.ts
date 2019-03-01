732bb5fd931059b58cb40722e9a8df61089bbf2f
kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-01 19:26:20 +0530

    tried of getting the param values

--- shopping-application/src/app/app.component.ts
+++ shopping-application/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shopping-application';
+export class AppComponent implements OnInit {
+    title = 'shopping-application';
+
+    constructor(private route: Router,
+                private router: ActivatedRoute) {}
+
+    ngOnInit() {
+        // console.log(this.route.url);
+        // this.router.parent.params.subscribe((params: Params) => {
+        //     // this.param = params['login'];
+        //     console.log(params);
+        // });
+    }
 }
+
