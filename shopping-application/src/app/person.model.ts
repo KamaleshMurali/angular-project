efd7a6169cc06b3e3231274d244c9ededb2798c2
kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-01 13:25:16 +0530

    model class has been created for person with address

---
+++ shopping-application/src/app/person.model.ts
@@ -0,0 +1,15 @@
+export class Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    dob: string;
+    address: Address;
+}
+export class Address {
+    flatNo: string;
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+}
