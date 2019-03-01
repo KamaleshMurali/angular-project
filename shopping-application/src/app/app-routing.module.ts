12ee5ad75fa0a5a3b028be0164c601aef72fe55d
kamalesh.murali <kamalesh.murali@object-frontier.com>
2019-03-01 19:27:00 +0530

    removed the commented code.

--- shopping-application/src/app/app-routing.module.ts
+++ shopping-application/src/app/app-routing.module.ts
@@ -4,18 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    // { path: 'home', component: HomeComponent, children: [
-        //     { path: 'offers', component: OffersComponent },
-        //     { path: 'mens', component: MensComponent},
-        //     { path: 'womens', component: WomensComponent}
-        // ]}
-        { path: '', redirectTo: 'login', pathMatch: 'full'},
-        { path: 'home', component: HomeComponent, children: [
-            { path: 'offers', loadChildren: './offers/offers.module#OffersModule'},
-            { path: 'mens', loadChildren: './mens/mens.module#MensModule'},
-            { path: 'womens', loadChildren: './womens/womens.module#WomensModule'}
-        ]},
-        { path: 'login', component: LoginComponent }
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent, children: [
+        { path: 'offers', loadChildren: './offers/offers.module#OffersModule'},
+        { path: 'mens', loadChildren: './mens/mens.module#MensModule'},
+        { path: 'womens', loadChildren: './womens/womens.module#WomensModule'}
+    ]},
+    { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
