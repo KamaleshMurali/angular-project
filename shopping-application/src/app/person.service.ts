77f11bd8c2561299c2148fa1ffc6cb630239e511
KamaleshMurali <31286936+KamaleshMurali@users.noreply.github.com>
2019-03-10 18:09:14 +0530

    used internal json for the data to populate

--- shopping-application/src/app/person.service.ts
+++ shopping-application/src/app/person.service.ts
@@ -9,12 +9,16 @@ export class PersonService {
 
     constructor(private httpClient: HttpClient) {}
 
-    getPerson(): Observable<any> {
-        // return this.httpClient.get('http://192.168.2.164:3000/users');
-        return this.httpClient.get('http://localhost:8080/spring-mvc-example/');
-    }
+    // getPerson(): Observable<any> {
+    //     // return this.httpClient.get('http://192.168.2.164:3000/users');
+    //     return this.httpClient.get('http://localhost:8080/spring-mvc-example/');
+    // }
 
     createPerson(person: Person): Observable<any> {
         return this.httpClient.post('http://localhost:8080/spring-mvc-example/post', person);
+    }
+
+    getPerson(): Observable<any> {
+        return this.httpClient.get('http://localhost:3000/users');
     }
 }
